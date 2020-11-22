d32b29a022a34a509577110149b235fd1786f6b5
isha-san <51543957+isha-san@users.noreply.github.com>
2020-11-21 21:58:19 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [caption, setCaption] = useState(0);
+
+  useEffect(() => {
+    fetch('/marissa').then(res => res.json()).then(data => {
+      setCaption(data.cap)
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{caption}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
