c361717b2561abbc746317c25494ee2ce16d08b6
sinsinsin66 <70784324+sinsinsin66@users.noreply.github.com>
2020-12-09 21:55:57 -0500

    Update ActionMenu.js

--- src/containers/ActionMenu.js
+++ src/containers/ActionMenu.js
@@ -1,7 +1,7 @@
 function Actionmenu(props) {
     return (
       <div className="action-menu">
-        <div id="shift-backward" onClick={props.carryOver}><div>&#xab;</div></div>
+        <div id="shift-backward" onClick={props.shiftBackward}><div>&#xab;</div></div>
         <div id="shift-forward" onClick={props.shiftForward}><div>&#187;</div></div>
         <div id="carry-over" onClick={props.carryOver}><div>&#8631;</div></div>
       </div>
